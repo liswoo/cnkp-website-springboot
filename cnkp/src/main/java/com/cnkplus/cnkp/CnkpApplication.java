4c9f9a89ff7606dccb0855e9b51a2025c6e4babf
liswoo <woosdsd@gmail.com>
2025-11-17 10:44:09 +0900

    Add Spring Boot project

---
+++ cnkp/src/main/java/com/cnkplus/cnkp/CnkpApplication.java
@@ -0,0 +1,13 @@
+package com.cnkplus.cnkp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnkpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnkpApplication.class, args);
+	}
+
+}
---
+++ cnkp/src/test/java/com/cnkplus/cnkp/CnkpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnkplus.cnkp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnkpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
